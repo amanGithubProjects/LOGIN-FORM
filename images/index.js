f5c167c494c7629423a9d2e7f2d8d5fb2fe734e3
amanGithubProjects <kaushik09909@gmail.com>
2021-03-13 19:49:24 +0530

    THIS IS MY SECOND PROJECT

---
+++ images/index.js
@@ -0,0 +1,11 @@
+function validate(){
+    var username=document.getElementById("username").Value;
+    var password=document.getElementById("password").value;
+    if(username=="admin" && password=="user"){
+        alert("login successfully");
+        return false;
+    }
+    else{
+        alert("login Declined");
+    }
+}
